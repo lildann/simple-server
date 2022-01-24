296fe77f096fcedf6a0e37e34d9d338a1b003c42
Lilly Daniell <daniell.lilly@gmail.com>
2022-01-24 10:28:53 +0000

    User can stay connected to server until 'exit'

--- simple_server.rb
+++ simple_server.rb
@@ -1,13 +1,16 @@
 require 'socket'
 
 server = TCPServer.new(2345)
-
 socket = server.accept
 
-socket.puts "What do you say?"
-
-they_said = socket.gets.chomp
-
-socket.puts "You said: #{they_said}. Goodbye!"
+while true do 
+  socket.puts "What do you say?"
+  they_said = socket.gets.chomp
+  if they_said == "exit"
+    break
+  else 
+    socket.puts "You said: #{they_said} !"
+  end
+end
 
 socket.close
